4332b56235e87c4e88c0331c2d2dfd0614432c25
418-teapot <scp000682@gmail.com>
2018-09-25 23:08:40 +0800

    fixed the format

--- src/cpu/define/segpos_def.v
+++ src/cpu/define/segpos_def.v
@@ -1,28 +1,28 @@
 // segment position
 
 // opcode
-`define SEG_OPCODE   31:26
+`define SEG_OPCODE      31:26
 
 // coprocessor
-`define SEG_CP0      25:21
+`define SEG_CP0         25:21
 
 // register segment
-`define SEG_RS       25:21
-`define SEG_RT       20:16
-`define SEG_RD       15:11
+`define SEG_RS          25:21
+`define SEG_RT          20:16
+`define SEG_RD          15:11
 
 // reg-imm
-`define SEG_REGIMM   20:16
+`define SEG_REGIMM      20:16
 
 // immediate or offset
-`define SEG_IMM      15:0
-`define SEG_OFFSET   15:0
+`define SEG_IMM         15:0
+`define SEG_OFFSET      15:0
 
 // address
-`define SEG_ADDR     25:0
+`define SEG_ADDR        25:0
 
 // shamt
-`define SEG_SHAMT    10:6
+`define SEG_SHAMT       10:6
 
 // funct
-`define SEG_FUNCT    5:0
+`define SEG_FUNCT       5:0
