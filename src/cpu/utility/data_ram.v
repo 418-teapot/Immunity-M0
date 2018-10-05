ef114cad322d5b965aba4a9b60310f037e189fff
418-teapot <scp000682@gmail.com>
2018-10-05 19:53:43 +0800

    fixed some bugs

--- src/cpu/utility/data_ram.v
+++ src/cpu/utility/data_ram.v
@@ -18,16 +18,19 @@ module  data_ram(
     output  reg [`DATA_BUS] read_data
 );
 
-    reg [`BYTE_BUS] data_mem    [`DATA_MEM_BUS];
+    reg [`BYTE_BUS] data_mem0[`DATA_MEM_BUS];
+    reg [`BYTE_BUS] data_mem1[`DATA_MEM_BUS];
+    reg [`BYTE_BUS] data_mem2[`DATA_MEM_BUS];
+    reg [`BYTE_BUS] data_mem3[`DATA_MEM_BUS];
 
     // write operation
     always @ (posedge clk)  begin
         if (ram_en == `CHIP_DISABLE)    begin
         end else if (write_en == `WRITE_ENABLE) begin
-            if (write_sel[3])   data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 0]    <= write_data[31:24];
-            if (write_sel[2])   data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 1]    <= write_data[23:16];
-            if (write_sel[1])   data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 2]    <= write_data[15: 8];
-            if (write_sel[0])   data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 3]    <= write_data[ 7: 0];
+            if (write_sel[3])   data_mem3[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]]   <= write_data[31:24];
+            if (write_sel[2])   data_mem2[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]]   <= write_data[23:16];
+            if (write_sel[1])   data_mem1[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]]   <= write_data[15: 8];
+            if (write_sel[0])   data_mem0[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]]   <= write_data[ 7: 0];
         end
     end
 
@@ -36,10 +39,10 @@ module  data_ram(
         if (ram_en == `CHIP_DISABLE)    
             read_data   <= `ZERO_WORD;
         else if (write_en == `WRITE_DISABLE)
-            read_data   <= {data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 0],
-                            data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 1], 
-                            data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 2],
-                            data_mem[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2] + 3]};
+            read_data   <= {data_mem3[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]],
+                            data_mem2[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]], 
+                            data_mem1[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]],
+                            data_mem0[write_addr[`DATA_MEM_NUM_LOG2 + 1 : 2]]};
         else
             read_data   <= `ZERO_WORD;
     end
